af1b5661e8742a03e58f49225c85cbe0d8f32608
lvyile <lvyile@cocoachina.com>
2012-11-30 02:31:14 +0800

    精简js

--- downloadTest.js
+++ downloadTest.js
@@ -1,35 +1,23 @@
 var Interface = Class(aBase,
-                      {
-                   onCreate : function(){
-                      this.downLoadBar = null;
-                   },
-                   
-                   init : function(){
-//*
-                      this.downLoadBar = new LoadingBar();
-                      this.downLoadBar.init();
-                      this.downLoadBar.setTexture("bar.png");
-                      var barPos = new UIPoint();
-                      barPos.x = 0;
-                      barPos.y = 280;
-                      this.downLoadBar.setPosition(barPos);
-                      this.downLoadBar.setPercent(80);
-                      uiSystem.m_pCurScene.addWidget(this.downLoadBar);
-                      cc.log("load loadingbar");
-                     //*/ 
-                      this.label = new Label();
-                      this.label.init();
-                      var labelPos = new UIPoint();
-                      labelPos.x = 240;
-                      labelPos.y = 280;
-                      this.label.setPosition(labelPos);
-                      this.label.setText("下载js完成之后的label ");
-                      this.label.setFontSize(20);
-                      this.label.setBeTouchAble(true);
-                      this.label.setTouchScaleChangeAble(true);
-                      uiSystem.m_pCurScene.addWidget(this.label);
-                      cc.log("load label....");                      
-                   }
-                   });
+                  {
+               onCreate : function(){
+                  this.downLoadBar = null;
+               },
+               
+               init : function(){
+                  this.downLoadBar = (new LoadingBar({x:150, y:280})).init();
+                  this.downLoadBar.setTexture("bar.png");
+                  this.downLoadBar.setPercent(0);
+                  uiSystem.m_pCurScene.addWidget(this.downLoadBar);
+
+                this.label3 = (new Label({x:240,y:280})).init();
+                this.label3.setText("我是下载js完成之后的label, 点击右侧开始下载大型文件，测断点续传->");
+                this.label3.setFontSize(18);
+                this.label3.setBeTouchAble(true);
+
+                  uiSystem.m_pCurScene.addWidget(this.label3);
+                  cc.log("load label....");                      
+               }
+               });
 
 var gInterface = new Interface();
